2ddf84b04fb91a9c4ad9d4dd7935b659a7f69c81
d4160 <d4160@outlook.com>
2021-02-14 20:44:27 -0500

    Added support for Vector2Int in MinMaxSlider Attribute

--- Scripts/Editor/PropertyDrawers/MinMaxSliderPropertyDrawer.cs
+++ Scripts/Editor/PropertyDrawers/MinMaxSliderPropertyDrawer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace NaughtyAttributes.Editor
 {
@@ -8,18 +9,18 @@ namespace NaughtyAttributes.Editor
 	{
 		protected override float GetPropertyHeight_Internal(SerializedProperty property, GUIContent label)
 		{
-			return (property.propertyType == SerializedPropertyType.Vector2)
+			return (property.propertyType == SerializedPropertyType.Vector2 || property.propertyType == SerializedPropertyType.Vector2Int)
 				? GetPropertyHeight(property)
 				: GetPropertyHeight(property) + GetHelpBoxHeight();
 		}
 
-		protected override void OnGUI_Internal(Rect rect, SerializedProperty property, GUIContent label)
+        protected override void OnGUI_Internal(Rect rect, SerializedProperty property, GUIContent label)
 		{
 			EditorGUI.BeginProperty(rect, label, property);
 
 			MinMaxSliderAttribute minMaxSliderAttribute = (MinMaxSliderAttribute)attribute;
 
-			if (property.propertyType == SerializedPropertyType.Vector2)
+			if (property.propertyType == SerializedPropertyType.Vector2 || property.propertyType == SerializedPropertyType.Vector2Int)
 			{
 				EditorGUI.BeginProperty(rect, label, property);
 
@@ -29,7 +30,7 @@ namespace NaughtyAttributes.Editor
 				float sliderWidth = rect.width - labelWidth - 2.0f * floatFieldWidth;
 				float sliderPadding = 5.0f;
 
-				Rect labelRect = new Rect(
+                Rect labelRect = new Rect(
 					rect.x,
 					rect.y,
 					labelWidth,
@@ -59,23 +60,29 @@ namespace NaughtyAttributes.Editor
 				// Draw the slider
 				EditorGUI.BeginChangeCheck();
 
-				Vector2 sliderValue = property.vector2Value;
-				EditorGUI.MinMaxSlider(sliderRect, ref sliderValue.x, ref sliderValue.y, minMaxSliderAttribute.MinValue, minMaxSliderAttribute.MaxValue);
+                Vector2 sliderValue = property.propertyType == SerializedPropertyType.Vector2 ? property.vector2Value : property.vector2IntValue;
+                EditorGUI.MinMaxSlider(sliderRect, ref sliderValue.x, ref sliderValue.y,
+                    minMaxSliderAttribute.MinValue, minMaxSliderAttribute.MaxValue);
 
-				sliderValue.x = EditorGUI.FloatField(minFloatFieldRect, sliderValue.x);
-				sliderValue.x = Mathf.Clamp(sliderValue.x, minMaxSliderAttribute.MinValue, Mathf.Min(minMaxSliderAttribute.MaxValue, sliderValue.y));
+                sliderValue.x = EditorGUI.FloatField(minFloatFieldRect, sliderValue.x);
+                sliderValue.x = Mathf.Clamp(sliderValue.x, minMaxSliderAttribute.MinValue,
+                    Mathf.Min(minMaxSliderAttribute.MaxValue, sliderValue.y));
 
-				sliderValue.y = EditorGUI.FloatField(maxFloatFieldRect, sliderValue.y);
-				sliderValue.y = Mathf.Clamp(sliderValue.y, Mathf.Max(minMaxSliderAttribute.MinValue, sliderValue.x), minMaxSliderAttribute.MaxValue);
+                sliderValue.y = EditorGUI.FloatField(maxFloatFieldRect, sliderValue.y);
+                sliderValue.y = Mathf.Clamp(sliderValue.y, Mathf.Max(minMaxSliderAttribute.MinValue, sliderValue.x),
+                    minMaxSliderAttribute.MaxValue);
 
-				if (EditorGUI.EndChangeCheck())
-				{
-					property.vector2Value = sliderValue;
+                if (EditorGUI.EndChangeCheck())
+                {
+					if(property.propertyType == SerializedPropertyType.Vector2)
+                        property.vector2Value = sliderValue;
+					else
+                        property.vector2IntValue = new Vector2Int((int)sliderValue.x, (int)sliderValue.y);
 				}
 
 				EditorGUI.EndProperty();
 			}
-			else
+            else
 			{
 				string message = minMaxSliderAttribute.GetType().Name + " can be used only on Vector2 fields";
 				DrawDefaultPropertyAndHelpBox(rect, property, message, MessageType.Warning);
